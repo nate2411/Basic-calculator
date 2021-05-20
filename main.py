c0a6cd6e2f4de9b3fcbc0717bc5d343a91b6c8cb
nate2411 <Nathandj28@gmail.com>
2021-05-20 15:53:09 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+#Basic Calculater
+
+class Add:
+    def __init__(self):
+        pass
+    def add(self, a, b):
+        return a + b
+
+
+class Subtract:
+    def __init__(self):
+        pass
+    def subtract(self, a, b):
+        return a - b
+
+
+class Divide:
+    def __init__(self):
+        pass
+    def divide(self, a, b):
+        return a / b
+
+
+class Multiply:
+    def __init__(self):
+        pass
+    def multiply(self, a, b):
+        return a * b
---
+++ mini.py
@@ -0,0 +1,7 @@
+import unittest
+from main import Add
+class TestAdd(unittest.TestCase):
+
+    def tset_add(self):
+        a_method = Add()
+        self.assertEqual(2, a_method.add(1, 1), "Adding 1 to 1 should be 2.")
